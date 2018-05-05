71d237604277ecf17bd7d567c0dcec4511fa2d50
ahmedtarek2376 <ahmed.tarek.2376@gmail.com>
2018-05-05 20:27:35 +0200

    try again

--- src/main/java/com/sdc/callingapp/tripcarmanagement/TripCarManagementApplication.java
+++ src/main/java/com/sdc/callingapp/tripcarmanagement/TripCarManagementApplication.java
@@ -1,10 +1,13 @@
 package com.sdc.callingapp.tripcarmanagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @ComponentScan("com.sdc.callingapp.tripcarmanagement")
 public class TripCarManagementApplication {
 
